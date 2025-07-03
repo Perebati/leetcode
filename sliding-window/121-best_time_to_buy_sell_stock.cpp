e5fdff9af9fb0c4d0b9a96622085c25512c912ea
Lucas Batista Pereira <lucas.bpereira1999@gmail.com>
2025-07-03 20:59:18 -0300

    daily challenge

---
+++ sliding-window/121-best_time_to_buy_sell_stock.cpp
@@ -0,0 +1,21 @@
+//link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int l = 0, r = 1;
+        int maxProfit = 0;
+
+        while(r < prices.size()){
+            if(prices[l] < prices[r]){
+                int profit = prices[r] - prices[l];
+                maxProfit = max(profit, maxProfit);
+            } else {
+                l = r;
+            }
+            r++;
+        }
+
+        return maxProfit;
+    }
+};
